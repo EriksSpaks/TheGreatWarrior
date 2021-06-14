e7c23e2def1674c9d2589200eead768ab3300353
eriks <erikspaks@gmail.com>
2021-06-14 13:29:23 +0300

    TheGreatWarrior

---
+++ src/com/company/World.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class World{
+    private long day = 1;
+
+    public long getDay() {
+        return day;
+    }
+
+    public void setDay(long day) {
+        this.day = day;
+    }
+}
