e4c1b8e45a661e4e83f458dfeaf67ebfddeb3ab4
eriks <erikspaks@gmail.com>
2021-06-19 15:48:59 +0300

    Characters classes

--- src/com/company/Ranger.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class Ranger extends Character{
-    public Ranger(String race, String name) {
-        super(2, 0, 4, 1, 1, 1, 10, 0, race, name);
-    }
-}
--- src/com/company/Warrior.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class Warrior extends Character{
-    public Warrior(String race, String name) {
-        super(3, 0, 1, 3, 2, 0, 10, 0, race, name);
-    }
-}
