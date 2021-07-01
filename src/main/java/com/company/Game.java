aacced2f4841142c4c5fa09c74d63b0651d002f1
eriks <erikspaks@gmail.com>
2021-07-01 22:09:48 +0300

    Save and load game

--- src/main/java/com/company/Game.java
+++ src/main/java/com/company/Game.java
@@ -20,6 +20,7 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Game {
@@ -29,6 +30,50 @@ public class Game {
         final String PATH = path + character.getName() + ".json";
 
         JSONObject jsonObject = new JSONObject();
+        HashMap<String, Boolean> areItemsAdded = new HashMap<>();
+        areItemsAdded.put("first wooden sword added", character.isFirstWoodenSwordAdded());
+        areItemsAdded.put("first bronze sword added", character.isFirstBronzeSwordAdded());
+        areItemsAdded.put("first iron sword added", character.isFirstIronSwordAdded());
+        areItemsAdded.put("first gold sword added", character.isFirstGoldSwordAdded());
+        areItemsAdded.put("first legendary sword added", character.isFirstLegendarySwordAdded());
+        areItemsAdded.put("first wooden bow added", character.isFirstWoodenBowAdded());
+        areItemsAdded.put("first bronze bow added", character.isFirstBronzeBowAdded());
+        areItemsAdded.put("first iron bow added", character.isFirstIronBowAdded());
+        areItemsAdded.put("first gold bow added", character.isFirstGoldBowAdded());
+        areItemsAdded.put("first legendary bow added", character.isFirstLegendaryBowAdded());
+        areItemsAdded.put("first wooden wand added", character.isFirstWoodenWandAdded());
+        areItemsAdded.put("first bronze wand added", character.isFirstBronzeWandAdded());
+        areItemsAdded.put("first iron wand added", character.isFirstIronWandAdded());
+        areItemsAdded.put("first gold wand added", character.isFirstGoldWandAdded());
+        areItemsAdded.put("first legendary wand added", character.isFirstLegendaryWandAdded());
+        areItemsAdded.put("first wooden shield added", character.isFirstWoodenShieldAdded());
+        areItemsAdded.put("first bronze shield added", character.isFirstBronzeShieldAdded());
+        areItemsAdded.put("first iron shield added", character.isFirstIronShieldAdded());
+        areItemsAdded.put("first gold shield added", character.isFirstGoldShieldAdded());
+        areItemsAdded.put("first legendary shield added", character.isFirstLegendaryShieldAdded());
+
+        HashMap<String, Long> itemStrengthAndDefense = new HashMap<>();
+        itemStrengthAndDefense.put("wooden sword strength", character.getWoodenSwordStrength());
+        itemStrengthAndDefense.put("bronze sword strength", character.getBronzeSwordStrength());
+        itemStrengthAndDefense.put("iron sword strength", character.getIronSwordStrength());
+        itemStrengthAndDefense.put("gold sword strength", character.getGoldSwordStrength());
+        itemStrengthAndDefense.put("legendary sword strength", character.getLegendarySwordStrength());
+        itemStrengthAndDefense.put("wooden bow strength", character.getWoodenBowStrength());
+        itemStrengthAndDefense.put("bronze bow strength", character.getBronzeBowStrength());
+        itemStrengthAndDefense.put("iron bow strength", character.getIronBowStrength());
+        itemStrengthAndDefense.put("gold bow strength", character.getGoldBowStrength());
+        itemStrengthAndDefense.put("legendary bow strength", character.getLegendaryBowStrength());
+        itemStrengthAndDefense.put("wooden wand intelligence", character.getWoodenWandIntelligence());
+        itemStrengthAndDefense.put("bronze wand intelligence", character.getBronzeWandIntelligence());
+        itemStrengthAndDefense.put("iron wand intelligence", character.getIronWandIntelligence());
+        itemStrengthAndDefense.put("gold wand intelligence", character.getGoldWandIntelligence());
+        itemStrengthAndDefense.put("legendary wand intelligence", character.getLegendaryWandIntelligence());
+        itemStrengthAndDefense.put("wooden shield defense", character.getWoodenShieldDefense());
+        itemStrengthAndDefense.put("bronze shield defense", character.getBronzeShieldDefense());
+        itemStrengthAndDefense.put("iron shield defense", character.getIronShieldDefense());
+        itemStrengthAndDefense.put("gold shield defense", character.getGoldShieldDefense());
+        itemStrengthAndDefense.put("legendary shield defense", character.getLegendaryShieldDefense());
+
         jsonObject.put("strength", character.getStrength());
         jsonObject.put("intelligence", character.getIntelligence());
         jsonObject.put("defense", character.getDefense());
@@ -43,6 +88,8 @@ public class Game {
         jsonObject.put("race", character.getRace());
         jsonObject.put("class", character.getClassification());
         jsonObject.put("xp to next level", character.getXpToNextLvl());
+        jsonObject.put("are items added first time", areItemsAdded);
+        jsonObject.put("item strength and defense", itemStrengthAndDefense);
 
         PrintWriter pw = new PrintWriter(PATH);
         pw.write(jsonObject.toJSONString());
@@ -311,6 +358,8 @@ public class Game {
 
         Object obj = new JSONParser().parse(new FileReader(path + name.get(characterNumber-1)));
         JSONObject jsonObject = (JSONObject) obj;
+        JSONObject areItemsAdded = (JSONObject) jsonObject.get("are items added first time");
+        JSONObject itemStrengthAndDefense = (JSONObject) jsonObject.get("item strength and defense");
 
         character.setLevel((long) jsonObject.get("level"));
         character.setIntelligence((long) jsonObject.get("intelligence"));
@@ -325,6 +374,49 @@ public class Game {
         character.setName((String) jsonObject.get("name"));
         character.setClassification((String) jsonObject.get("class"));
         character.setXpToNextLvl((long) jsonObject.get("xp to next level"));
+
+        character.setFirstWoodenSwordAdded((Boolean) areItemsAdded.get("first wooden sword added"));
+        character.setFirstBronzeSwordAdded((Boolean) areItemsAdded.get("first bronze sword added"));
+        character.setFirstIronSwordAdded((Boolean) areItemsAdded.get("first iron sword added"));
+        character.setFirstGoldSwordAdded((Boolean) areItemsAdded.get("first gold sword added"));
+        character.setFirstLegendarySwordAdded((Boolean) areItemsAdded.get("first legendary sword added"));
+        character.setFirstWoodenBowAdded((Boolean) areItemsAdded.get("first wooden bow added"));
+        character.setFirstBronzeBowAdded((Boolean) areItemsAdded.get("first bronze bow added"));
+        character.setFirstIronBowAdded((Boolean) areItemsAdded.get("first iron bow added"));
+        character.setFirstGoldBowAdded((Boolean) areItemsAdded.get("first gold bow added"));
+        character.setFirstLegendaryBowAdded((Boolean) areItemsAdded.get("first legendary bow added"));
+        character.setFirstWoodenWandAdded((Boolean) areItemsAdded.get("first wooden wand added"));
+        character.setFirstBronzeWandAdded((Boolean) areItemsAdded.get("first bronze wand added"));
+        character.setFirstIronWandAdded((Boolean) areItemsAdded.get("first iron wand added"));
+        character.setFirstGoldWandAdded((Boolean) areItemsAdded.get("first gold wand added"));
+        character.setFirstLegendaryWandAdded((Boolean) areItemsAdded.get("first legendary wand added"));
+        character.setFirstWoodenShieldAdded((Boolean) areItemsAdded.get("first wooden shield added"));
+        character.setFirstBronzeShieldAdded((Boolean) areItemsAdded.get("first bronze shield added"));
+        character.setFirstIronShieldAdded((Boolean) areItemsAdded.get("first iron shield added"));
+        character.setFirstGoldShieldAdded((Boolean) areItemsAdded.get("first gold shield added"));
+        character.setFirstLegendaryShieldAdded((Boolean) areItemsAdded.get("first legendary shield added"));
+
+        character.setWoodenSwordStrength((Long) itemStrengthAndDefense.get("wooden sword strength"));
+        character.setBronzeSwordStrength((Long) itemStrengthAndDefense.get("bronze sword strength"));
+        character.setIronSwordStrength((Long) itemStrengthAndDefense.get("iron sword strength"));
+        character.setGoldSwordStrength((Long) itemStrengthAndDefense.get("gold sword strength"));
+        character.setLegendarySwordStrength((Long) itemStrengthAndDefense.get("legendary sword strength"));
+        character.setWoodenBowStrength((Long) itemStrengthAndDefense.get("wooden bow strength"));
+        character.setBronzeBowStrength((Long) itemStrengthAndDefense.get("bronze bow strength"));
+        character.setIronBowStrength((Long) itemStrengthAndDefense.get("iron bow strength"));
+        character.setGoldBowStrength((Long) itemStrengthAndDefense.get("gold bow strength"));
+        character.setLegendaryBowStrength((Long) itemStrengthAndDefense.get("legendary bow strength"));
+        character.setWoodenWandIntelligence((Long) itemStrengthAndDefense.get("wooden wand intelligence"));
+        character.setBronzeWandIntelligence((Long) itemStrengthAndDefense.get("bronze wand intelligence"));
+        character.setIronWandIntelligence((Long) itemStrengthAndDefense.get("iron wand intelligence"));
+        character.setGoldWandIntelligence((Long) itemStrengthAndDefense.get("gold wand intelligence"));
+        character.setLegendaryWandIntelligence((Long) itemStrengthAndDefense.get("legendary wand intelligence"));
+        character.setWoodenShieldDefense((Long) itemStrengthAndDefense.get("wooden shield defense"));
+        character.setBronzeShieldDefense((Long) itemStrengthAndDefense.get("bronze shield defense"));
+        character.setIronShieldDefense((Long) itemStrengthAndDefense.get("iron shield defense"));
+        character.setGoldShieldDefense((Long) itemStrengthAndDefense.get("gold shield defense"));
+        character.setLegendaryShieldDefense((Long) itemStrengthAndDefense.get("legendary shield defense"));
+
         world.setDay((long) jsonObject.get("world day"));
 
         return character;
